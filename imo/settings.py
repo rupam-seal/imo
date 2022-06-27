c99b81387d80f76f6d90d9b30c8b05e401527ce1
rupam-seal <rupam.x.seal@gmail.com>
2022-06-27 14:08:09 +0530

    modified code

--- imo/settings.py
+++ imo/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()  # loads the configs from .env
 
 import tensorflow.compat.v1 as tf
 from keras.applications import vgg16
@@ -35,7 +37,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SECRET_KEY'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
