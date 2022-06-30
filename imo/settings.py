f779311e4da1754b34f7b73d714c68284d333b6c
rupam-seal <rupam.x.seal@gmail.com>
2022-06-30 16:02:46 +0530

    added vercel settings

--- imo/settings.py
+++ imo/settings.py
@@ -140,7 +140,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
